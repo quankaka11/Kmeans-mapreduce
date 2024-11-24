42410035bea18e47e9d72b5f4d86447153807b64
quankaka11 <144087570+quankaka11@users.noreply.github.com>
2024-11-24 22:11:35 +0700

    Loai bo can bac trong khi tinh khoang cach EuclideanDistance.java

--- src/distances/EuclideanDistance.java
+++ src/distances/EuclideanDistance.java
@@ -7,16 +7,17 @@ public class EuclideanDistance extends Distance {
     public double getDistance(Point p1, Point p2) throws Exception {
         double[] p1Vector = p1.getVector();
         double[] p2Vector = p2.getVector();
-
+    
         if (p1Vector.length != p2Vector.length) throw new Exception("Invalid length");
-
-        double sum = 0f;
-        for (int i = 0; i < p1Vector.length; i++){
+    
+        double sum = 0;
+        for (int i = 0; i < p1Vector.length; i++) {
             sum += Math.pow(p1Vector[i] - p2Vector[i], 2);
         }
-
-        return Math.sqrt(sum);
+    
+        return sum; 
     }
+
 
     @Override
     public Point getExpectation(Iterable<Point> points) {
